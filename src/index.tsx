88aaaa455e3077dbc0cf3f63b9a011f1285363c7
qianyan <836938311@qq.com>
2022-07-25 19:12:46 +0800

    achange

--- src/index.tsx
+++ src/index.tsx
@@ -25,7 +25,7 @@ export default class extends WeElement<MyAppProps> {
   render(props) {
     return (
       <div class="app">
-        <o-transfer data={[{key: 1 , table: "1"},{key: 2, table : "2"}]}></o-transfer>
+        <o-transfer data={[{key: 1 , table: "1"},{key: 2, table : "2"},{key: 3 , table: "3"} ,{key: 4 , table: "4"} ]}></o-transfer>
       </div>
     )
   }
